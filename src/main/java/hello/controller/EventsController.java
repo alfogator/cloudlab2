f7fc9db48db3976a4506f19052c0ca094bc4da55
Alfonso Caschili <alfo@gator.it>
2019-07-20 12:20:40 +0200

    added profiles

--- src/main/java/hello/controller/EventsController.java
+++ src/main/java/hello/controller/EventsController.java
@@ -23,5 +23,54 @@ public class EventsController {
         return eventsRepository.findAll();
     }
 
+    // GET /events            tutti gli eventi
+    // GET /events/1          evento con id 1
+    // POST /events           crea nuovo evento
+    // PUT /events/1          update evento 1
+    // DELETE /events/1       cancella evento 1
+
+
+
+    @GetMapping("/events/{id}")
+    public Event fetchEvent(@PathVariable long id) {
+        Optional<Event> event = eventsRepository.findById(id);
+
+        if (!event.isPresent())
+            throw new EventNotFoundException();
+
+        return event.get();
+    }
+
+    @DeleteMapping("/events/{id}")
+    public void deleteEvent(@PathVariable long id) {
+        eventsRepository.deleteById(id);
+    }
+
+    @PostMapping("/events")
+    public ResponseEntity<Object> createEvent(@RequestBody Event event) {
+        Event savedEvent = eventsRepository.save(event);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(savedEvent.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+
+    }
+
+    @PutMapping("/events/{id}")
+    public ResponseEntity<Object> updateEvent(@RequestBody Event event, @PathVariable long id) {
+
+        Optional<Event> eventOptional = eventsRepository.findById(id);
+
+        if (!eventOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
+        event.setId(id);
+
+        eventsRepository.save(event);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
 
